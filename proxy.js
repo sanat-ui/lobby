e84f731a7acf5d7fb250ac53d075c5cadc0a2182
sanat <sanatmuaythai@gmail.com>
2026-08-06 16:00:16 +0530

    Fix proxy.js export

--- proxy.js
+++ proxy.js
@@ -1,12 +1,5 @@
-// middleware.js
 import { NextResponse } from 'next/server'
 
-export function middleware(request) {
-  // We'll add proper auth protection in Phase 5
-  // For now just let all requests through
+export function proxy() {
   return NextResponse.next()
-}
-
-export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)']
 }
